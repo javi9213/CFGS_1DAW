cc11b5ed0da3d3ecb0cf1414db8f094b8ef2b96c
javi9213 <javi.9213@gmail.com>
2013-01-27 20:42:44 +0100

    q2

---
+++ Proyectos/Java11-12/JParejas/src/JParejas/JParejasActivity.java
@@ -0,0 +1,14 @@
+package JParejas;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JParejasActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_content);
+    }
+}
